d0f7996e4d3f7c6b741cb7d402a370b55f59b346
Hady Osman <hady.osman@xero.com>
2014-08-04 18:49:06 +1200

    Exclude node_modules from build bundle and add to git ignore

--- after_success.sh
+++ after_success.sh
@@ -1,5 +1,7 @@
 echo -e "Current Repo:$REPO --- Travis Branch:$TRAVIS_BRANCH"
 
+# TODO: Extract version from addon.xml
+
 # Create binary ZIP file
-# Ignore all hidden files, .PSD, tests and requirements.txt files
-zip -r plugin.video.dailytube4u.com-2.0.0.zip . -x '*.git*' '\.*' '*/\.*' '*.sh' '*.psd' 'resources/tests/*' '*.pyc' 'requirements.txt'
+# Ignore all hidden files, .PSD, tests, node_modules and requirements.txt files
+zip -r plugin.video.dailytube4u.com-2.0.0.zip . -x '*.git*' '\.*' '*/\.*' '*.sh' '*.psd' 'resources/tests/*' '*.pyc' 'requirements.txt' 'node_modules/*'
