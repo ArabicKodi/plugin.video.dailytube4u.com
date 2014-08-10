fb35220561c1fec381db2a7c48bc48eb9389df41
Hady Osman <hady.osman@xero.com>
2014-08-10 20:00:35 +1200

    Removed conditional `tags=true` requirement.

--- after_success.sh
+++ after_success.sh
@@ -3,11 +3,6 @@ echo -e "Current Repo:$REPO --- Travis Branch:$TRAVIS_BRANCH"
 
 # TODO: Extract version from addon.xml
 
-echo "Debugging messages..."
-echo "\$REPO" $REPO
-echo "\$REPO.git" $REPO.git
-git remote -v
-
 # Ignore all hidden files, .PSD, tests, node_modules and requirements.txt files
 echo "Creating XBMC plugin zip file"
 zip -r "plugin.video.dailytube4u.com-${TRAVIS_BRANCH}.zip" . -x '*.git*' '\.*' '*/\.*' '*.sh' '*.psd' 'resources/tests/*' '*.pyc' 'requirements.txt' 'node_modules/*'
