03c7a2ef32130070f82f17cdb5aa7d08342ee24e
Hady Osman <hady.osman@xero.com>
2014-08-10 14:12:44 +1200

    Add some more debug messages to determine git repository URL on Travis

--- after_success.sh
+++ after_success.sh
@@ -3,6 +3,11 @@ echo -e "Current Repo:$REPO --- Travis Branch:$TRAVIS_BRANCH"
 
 # TODO: Extract version from addon.xml
 
+echo "Debugging messages..."
+echo "\$REPO" $REPO
+echo "\$REPO.git" $REPO.git
+
+
 # Ignore all hidden files, .PSD, tests, node_modules and requirements.txt files
 echo "Creating XBMC plugin zip file"
 zip -r plugin.video.dailytube4u.com.zip . -x '*.git*' '\.*' '*/\.*' '*.sh' '*.psd' 'resources/tests/*' '*.pyc' 'requirements.txt' 'node_modules/*'
